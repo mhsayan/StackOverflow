9e35a998c3d6eef9be87f34460c4de1cf2b0bb48
Md. Mahmudul Hasan <mhsayan.ltd@gmail.com>
2022-01-14 11:12:16 +0600

    Added question controller with views

---
+++ StackOverflow/StackOverflow.Web/Controllers/QuestionController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace StackOverflow.Web.Controllers
+{
+    public class QuestionController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Details()
+        {
+            return View();
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        public IActionResult Edit()
+        {
+            return View();
+        }
+
+        public IActionResult Delete()
+        {
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
